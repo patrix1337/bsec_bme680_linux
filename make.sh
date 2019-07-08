430b8fe38abe3f8bfb97ca865ccb0c4c7646d216
Chris Crocker-White <chriscw@balena.io>
2019-07-08 12:17:13 +0100

    Updated to latest BSEC version inc folder structure changes
    Change-type: patch
    Signed-off-by: Chris Crocker-White <chriscw@balena.io>

--- make.sh
+++ make.sh
@@ -24,15 +24,15 @@ echo 'Compiling...'
 cc -Wall -Wno-unused-but-set-variable -Wno-unused-variable -static \
   -std=c99 -pedantic \
   -iquote"${BSEC_DIR}"/API \
-  -iquote"${BSEC_DIR}"/algo/bin/${ARCH} \
+  -iquote"${BSEC_DIR}"/algo/${ARCH} \
   -iquote"${BSEC_DIR}"/examples \
   "${BSEC_DIR}"/API/bme680.c \
   "${BSEC_DIR}"/examples/bsec_integration.c \
   ./bsec_bme680.c \
-  -L"${BSEC_DIR}"/algo/bin/"${ARCH}" -lalgobsec \
+  -L"${BSEC_DIR}"/algo/"${ARCH}" -lalgobsec \
   -lm -lrt \
   -o bsec_bme680
 echo 'Compiled.'
 
-cp "${BSEC_DIR}"/config/"${CONFIG}"/bsec_iaq.config ./	
+cp "${BSEC_DIR}"/config/"${CONFIG}"/bsec_iaq.config ./
 echo 'Copied config.'
